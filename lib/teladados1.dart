e8eaf3c87cf597a7b9bb7a73de92b05ed7a22d0f
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2022-11-02 17:55:00 -0300

    atribuindo função

--- lib/teladados1.dart
+++ lib/teladados1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/telaexpanded.dart';
 
 class TelaDados1 extends StatelessWidget {
   const TelaDados1({Key? key}) : super(key: key);
@@ -22,12 +23,11 @@ class TelaDados1 extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-
         child: Center(
           child: Card(
             color: const Color.fromARGB(255, 39, 46, 91).withOpacity(0.8),
             elevation: 90,
-            margin: const EdgeInsets.all(50),  
+            margin: const EdgeInsets.all(50),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
@@ -36,7 +36,6 @@ class TelaDados1 extends StatelessWidget {
               children: <Widget>[
                 const ListTile(
                   leading: Icon(Icons.home),
-                  
                   title: Text('Imóvel'),
                   subtitle: Text('Valor diária: RS 100,00'),
                   iconColor: Color.fromARGB(255, 233, 233, 233),
@@ -47,14 +46,12 @@ class TelaDados1 extends StatelessWidget {
                   iconColor: Color.fromARGB(255, 233, 233, 233),
                   title: Text('Endereço: R. Florêncio Joaquim Rodrigues, 419'),
                   textColor: Color.fromARGB(255, 233, 233, 233),
-
                 ),
                 const ListTile(
                   leading: Icon(Icons.phone),
                   title: Text('Telefone: (89) 994 5640-10'),
                   iconColor: Color.fromARGB(255, 233, 233, 233),
                   textColor: Color.fromARGB(255, 233, 233, 233),
-
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -66,9 +63,15 @@ class TelaDados1 extends StatelessWidget {
                     const SizedBox(width: 8),
                     TextButton(
                       child: const Text('Cancelar'),
-                      onPressed: () {/* ... */},
+                      onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TelaExpanded()),
+                        )
+                      },
                     ),
-                    const SizedBox(width: 8),                    
+                    const SizedBox(width: 8),
                   ],
                 ),
               ],
@@ -79,6 +82,3 @@ class TelaDados1 extends StatelessWidget {
     );
   }
 }
-
-
-
